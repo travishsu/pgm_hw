2b1246c94f481c01b8cc9e3a433ffe0cdd74b98d
Yichung Hsu <fov223@gmail.com>
2016-10-02 09:45:49 +0800

    complete week1 and create dir

--- week1/ComputeMarginal.m
+++ week1/ComputeMarginal.m
@@ -2,12 +2,12 @@
 %   M = ComputeMarginal(V, F, E) computes the marginal over variables V
 %   in the distribution induced by the set of factors F, given evidence E
 %
-%   M is a factor containing the marginal over variables V
+%   M is a factor containing the marginal over variables V, M = P()
 %   V is a vector containing the variables in the marginal e.g. [1 2 3] for
 %     X_1, X_2 and X_3.
-%   F is a vector of factors (struct array) containing the factors 
+%   F is a vector of factors (struct array) containing the factors
 %     defining the distribution
-%   E is an N-by-2 matrix, each row being a variable/value pair. 
+%   E is an N-by-2 matrix, each row being a variable/value pair.
 %     Variables are in the first column and values are in the second column.
 %     If there is no evidence, pass in the empty matrix [] for E.
 
@@ -17,7 +17,7 @@ function M = ComputeMarginal(V, F, E)
 % Check for empty factor list
 if (numel(F) == 0)
       warning('Warning: empty factor list');
-      M = struct('var', [], 'card', [], 'val', []);      
+      M = struct('var', [], 'card', [], 'val', []);
       return;
 end
 
@@ -28,6 +28,13 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 M = struct('var', [], 'card', [], 'val', []); % Returns empty factor. Change this.
+
+p = ComputeJointDistribution(F);
+x = ObserveEvidence(p,E);
+y = FactorMarginalization(x,setdiff(x.var,V));
+M = y;
+M.val = y.val/(sum(y.val));
+M = M
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
